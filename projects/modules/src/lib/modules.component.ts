ab0043ad15223a35886d9f9e3fd938f55c4e4733
nvytychakdev <n.vytychak@gmail.com>
2025-03-29 11:53:48 +0200

    chore(project): initial project setup

---
+++ projects/modules/src/lib/modules.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-modules',
+  imports: [],
+  template: `
+    <p>
+      modules works!
+    </p>
+  `,
+  styles: ``
+})
+export class ModulesComponent {
+
+}
---
+++ projects/modules/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of modules
+ */
+
+export * from './lib/modules.service';
+export * from './lib/modules.component';
