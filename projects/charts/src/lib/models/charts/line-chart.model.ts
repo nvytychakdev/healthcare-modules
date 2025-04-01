bc6d549ad599b18582b6cc5ce643bd925daefff9
nvytychakdev <n.vytychak@gmail.com>
2025-04-01 23:58:04 +0300

    chore(charts): refactor line chart

--- projects/charts/src/lib/models/charts/line-chart.model.ts
+++ projects/charts/src/lib/models/charts/line-chart.model.ts
@@ -1,4 +1,5 @@
-import { Bullet, Circle, color, DataProcessor, Theme, Tooltip } from '@amcharts/amcharts5';
+import { Bullet, Circle, color, DataProcessor, Root, Theme, Tooltip } from '@amcharts/amcharts5';
+import { AxisRenderer } from '@amcharts/amcharts5/.internal/charts/xy/axes/AxisRenderer';
 import { AxisRendererX } from '@amcharts/amcharts5/.internal/charts/xy/axes/AxisRendererX';
 import { AxisRendererY } from '@amcharts/amcharts5/.internal/charts/xy/axes/AxisRendererY';
 import { DateAxis } from '@amcharts/amcharts5/.internal/charts/xy/axes/DateAxis';
@@ -11,32 +12,58 @@ import { ChartRender } from '../../interfaces/chart-render.interface';
 import { BaseChart } from '../base-chart.model';
 
 export class LineChart extends BaseChart {
+  private readonly themeColors = ['#82c535', '#087f8c', '#00ceb0'];
+
   override render(): ChartRender {
     const root = this.createRoot();
+    this.applyThemes(root);
 
+    const chart = this.createChart(root);
+    const xAxis = this.createXAxis(root, chart);
+    const yAxis = this.createYAxis(root, chart);
+    const series = this.createSeries(root, chart, xAxis, yAxis);
+
+    series.appear(1000);
+    chart.appear(1000, 100);
+
+    return { root, series, chart };
+  }
+
+  private applyThemes(root: Root): void {
     const theme = Theme.new(root);
-    theme.rule('ColorSet').set('colors', [color('#82c535'), color('#087f8c'), color('#00ceb0')]);
+    theme.rule('ColorSet').set('colors', this.themeColors.map(color));
 
     root.setThemes([AnimatedTheme.new(root), DarkTheme.new(root), theme]);
-    const chart = root.container.children.push(XYChart.new(root, {}));
+  }
 
-    const xAxis = chart.xAxes.push(
+  private createChart(root: Root): XYChart {
+    return root.container.children.push(XYChart.new(root, {}));
+  }
+
+  private createXAxis(root: Root, chart: XYChart): DateAxis<AxisRenderer> {
+    return chart.xAxes.push(
       DateAxis.new(root, {
-        baseInterval: {
-          timeUnit: 'day',
-          count: 1,
-        },
+        baseInterval: { timeUnit: 'day', count: 1 },
         renderer: AxisRendererX.new(root, {}),
         tooltip: Tooltip.new(root, {}),
       }),
     );
+  }
 
-    const yAxis = chart.yAxes.push(
+  private createYAxis(root: Root, chart: XYChart): ValueAxis<AxisRenderer> {
+    return chart.yAxes.push(
       ValueAxis.new(root, {
         renderer: AxisRendererY.new(root, {}),
       }),
     );
+  }
 
+  private createSeries(
+    root: Root,
+    chart: XYChart,
+    xAxis: DateAxis<AxisRenderer>,
+    yAxis: ValueAxis<AxisRenderer>,
+  ): LineSeries {
     const series = chart.series.push(
       LineSeries.new(root, {
         name: 'Series',
@@ -44,30 +71,31 @@ export class LineChart extends BaseChart {
         yAxis: yAxis,
         valueYField: this.fields.valueYField,
         valueXField: this.fields.valueXField,
-        tooltip: Tooltip.new(root, {
-          labelText: '{valueY}',
-        }),
+        tooltip: Tooltip.new(root, { labelText: '{valueY}' }),
       }),
     );
 
+    this.configureDataProcessor(root, series);
+    this.addBullets(root, series);
+
+    return series;
+  }
+
+  private configureDataProcessor(root: Root, series: LineSeries): void {
     series.data.processor = DataProcessor.new(root, {
       dateFormat: 'i',
       dateFields: [this.fields.valueXField],
     });
+  }
 
+  private addBullets(root: Root, series: LineSeries): void {
     series.bullets.push(() => {
-      let bulletCircle = Circle.new(root, {
+      const bulletCircle = Circle.new(root, {
         radius: 5,
         fill: series.get('fill'),
       });
-      return Bullet.new(root, {
-        sprite: bulletCircle,
-      });
+
+      return Bullet.new(root, { sprite: bulletCircle });
     });
-
-    series.appear(1000);
-    chart.appear(1000, 100);
-
-    return { root, series, chart };
   }
 }
